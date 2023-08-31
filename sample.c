9678f2dc2e3edd18afa8a51f30aff0f99a0ce24e
Prabhat Das <prabhatdash0376@gmail.com>
2023-08-31 15:13:44 +0530

    cmake

--- sample.c
+++ sample.c
@@ -1,14 +1,6 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
-    float a,b,c;
-    printf("ENTER THE VALUE OF a:\n");
-    scanf("%f",&a);
-    printf("ENTER THE VALUE OF b:\n");
-    scanf("%f",&b);
-    printf("ENTER THE VALUE OF c:\n");
-    scanf("%f",&c);
-    float sl_13;
-    sl_13=(a*a*a)+(b*b*b)+(c*c*c)-3*a*b*c;
-    printf("THE VALUE OF 13 NO IDENTITY IS: %f",sl_13);
-
+  float a=10;
+    printf("%f", pow(a,2));
 }
